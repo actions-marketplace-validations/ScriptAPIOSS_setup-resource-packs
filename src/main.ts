7af05156dee092d643ed83e20a3307dd2fa5069f
GTB3NW <GTB3NW@users.noreply.github.com>
2022-12-18 20:25:04 +0000

    debug

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import * as io from '@actions/io'
+import * as glob from '@actions/glob'
 
 import path from 'path'
 
@@ -21,9 +22,16 @@ async function run(): Promise<void> {
     const options = {recursive: true, force: false}
 
     for (const p of TARGET_SOURCE_PATHS) {
-      const base_dir = path.basename(p)
+      // const base_dir = path.basename(p)
+      const foo = path.join(p, "manifest.json")
+      const globber = await glob.create(foo)
+      const files = await globber.glob()
+
+      for (const f of files) {
+        core.info(`got: ${f}`)
+      }
       
-      await io.cp(p, `${TARGET_DEST_PATH}/${base_dir}`, options)
+      // await io.cp(p, `${TARGET_DEST_PATH}/${base_dir}`, options)
     }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
